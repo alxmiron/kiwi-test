4a0f2c71052a63bdd5fb3560b604f031e57cc94b
Oleksii Myronenko <oleksii.myronenko@crello.com>
2021-03-25 00:47:10 +0200

    Fix ApolloClient

--- client/withApollo.tsx
+++ client/withApollo.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent, ComponentType } from "react";
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache, split } from "@apollo/client";
 
 const link = createHttpLink({
-	uri: "http://localhost:3000/api/graphql",
+	uri: "/api/graphql",
 });
 
 const client = new ApolloClient({
